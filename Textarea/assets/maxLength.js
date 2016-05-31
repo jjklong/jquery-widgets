8d29d7223eefb92752bc3ec28be8775a0346a47a
Jordan Long <jordan.long222@gmail.com>
2016-05-31 17:14:42 -0400

    Add Textarea

---
+++ Textarea/assets/maxLength.js
@@ -0,0 +1,9 @@
+$(function () {
+  'use strict';
+});
+
+
+// 1) variables needed: maxLength, current length,
+// 2) select $textarea, add keyup event
+// 3) console.log(_ characters remaining)
+// 4) add value (remaining characters) + .text to HTML ("characters remaining")
