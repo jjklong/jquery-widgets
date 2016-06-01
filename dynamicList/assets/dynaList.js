1aa4975a7c037533d40d1b495cd8a13dc226897d
Jordan Long <jordan.long222@gmail.com>
2016-05-31 23:42:45 -0400

    jquery if else try

--- dynamicList/assets/dynaList.js
+++ dynamicList/assets/dynaList.js
@@ -4,14 +4,36 @@
 var x = 1;
 console.log(x);
 
-$('input').keyup(function() {
+
+//Trying different if else statements to remove input when no content
+$('input').keypress(function() {
   var check4txt = $('input').val().length;
   var txtmin = 1;
-  if (check4txt === 0){
-    $('input').removeClass('<input type="text" name="input_box">');
-  } else if (check4txt < txtmin){
-    $('input').removeClass('<input type="text" name="input_box">');
-  } else if (check4txt > txtmin){
-    $('input').after('<input type="text" name="input_box">');
+   if ((check4txt > txtmin) && (check4txt !== 0)){
+    $('input').after('<input type="text" name="input_box"><br>');
+  } else {
+    $('input:eq(1)').removeClass('<input type="text" name="input_box"><br>');
   }
 });
+//
+// $('input').focus(function() {
+//   var check4txt = $('input').val().length;
+//   var txtmin = 1;
+//   if (check4txt === 0){
+//     $('input').removeClass('<input type="text" name="input_box">');
+//   } else if (check4txt < txtmin){
+//     $('input').removeClass('<input type="text" name="input_box">');
+//   } else if (check4txt > txtmin){
+//     $('input').after('<input type="text" name="input_box">');
+//   }
+// });
+//
+// $('input').focus(function() {
+//   var check4txt = $('input').val().length;
+//   var txtmin = 1;
+//   if (check4txt > txtmin){
+//     $('input').after('<input type="text" name="input_box">');
+//   } else if (check4txt < txtmin) {
+//     $('input:eq(1)').removeClass();
+//   }
+// });
